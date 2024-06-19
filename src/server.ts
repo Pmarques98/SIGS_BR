ef3d324081e58f950986cc1df7b486b8a2ea9a67
Patrick Marques <patrick.marques@usp.br>
2024-06-19 16:51:27 -0300

    cadastro user

--- src/server.ts
+++ src/server.ts
@@ -24,4 +24,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 })
 
-app.listen(process.env.PORT || 3333,() => console.log("Servidor Online!!"))
+app.listen(process.env.PORT || 3333,() => console.log("Servidor Online"))
